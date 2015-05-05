ccf9fd5edc65dcd77de79619d107bed00f4d8d6a
txazo <784990655@qq.com>
2015-05-05 23:21:47 +0800

    txazo-util工具类

---
+++ txazo-util/src/main/java/org.txazo.util/ClassUtils.java
@@ -0,0 +1,13 @@
+package org.txazo.util;
+
+/**
+ * Class工具类
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @qq 784990655
+ * @since 05.05.2015
+ */
+public abstract class ClassUtils {
+
+}
---
+++ txazo-util/src/main/java/org.txazo.util/CollectionsUtils.java
@@ -0,0 +1,13 @@
+package org.txazo.util;
+
+/**
+ * 集合工具类
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @qq 784990655
+ * @since 05.05.2015
+ */
+public abstract class CollectionsUtils {
+
+}
