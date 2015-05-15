2283e83860c228159bdbf5bbb83752158b5b80e6
txazo <784990655@qq.com>
2015-05-15 18:41:29 +0800

    update

--- txazo-reflection/src/main/java/org/txazo/reflection/ReflectionClass.java
+++ txazo-reflection/src/main/java/org/txazo/reflection/ReflectionClass.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.txazo.reflection.vo.ClassVo;
 import org.txazo.test.AbstractTest;
 
+import java.lang.reflect.Field;
+
 /**
  * ReflectionClass
  *
@@ -15,14 +17,18 @@ import org.txazo.test.AbstractTest;
 public class ReflectionClass extends AbstractTest {
 
     @Test
-    public void test1() throws ClassNotFoundException {
+    public void test1() throws ClassNotFoundException, NoSuchFieldException {
+        /** class对象的获取 */
         Class<?> c1 = ClassVo.class;
         Class<?> c2 = new ClassVo().getClass();
         Class<?> c3 = Class.forName("org.txazo.reflection.vo.ClassVo");
         Class<?> c4 = Thread.currentThread().getContextClassLoader().loadClass("org.txazo.reflection.vo.ClassVo");
         Assert.assertSame(c1, c2);
-        Assert.assertSame(c1, c3);
         Assert.assertSame(c1, c4);
+
+        /** 基本数据类型的class */
+        Field field = c1.getField("id");
+        Assert.assertSame(field.getType(), int.class);
     }
 
     @Test
