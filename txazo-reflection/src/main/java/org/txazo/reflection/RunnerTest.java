ef11838a45c00d9758afe1e819f71a8bb8199f9e
txazo <784990655@qq.com>
2015-05-26 09:09:40 +0800

    txazo-reflection

---
+++ txazo-reflection/src/main/java/org/txazo/reflection/RunnerTest.java
@@ -0,0 +1,20 @@
+package org.txazo.reflection;
+
+import org.txazo.test.runner.SuiteTestRunner;
+import org.txazo.test.runner.TestRunner;
+
+/**
+ * RunnerTest
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @since 26.05.2015
+ */
+public class RunnerTest {
+
+    public static void main(String[] args) {
+        TestRunner runner = new SuiteTestRunner();
+        runner.run(RunnerTest.class.getPackage().getName(), true);
+    }
+
+}
