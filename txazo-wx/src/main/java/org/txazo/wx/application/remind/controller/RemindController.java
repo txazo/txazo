e0d242ba00c0a1389ad6c90eb69198b845cb90b9
txazo <784990655@qq.com>
2015-06-04 17:09:31 +0800

    update

---
+++ txazo-wx/src/main/java/org/txazo/wx/application/remind/controller/RemindController.java
@@ -0,0 +1,22 @@
+package org.txazo.wx.application.remind.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * IndexController
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @since 04.06.2015
+ */
+@Controller
+@RequestMapping("/remind")
+public class RemindController {
+
+    @RequestMapping("/add.wx")
+    public void add() {
+
+    }
+
+}
