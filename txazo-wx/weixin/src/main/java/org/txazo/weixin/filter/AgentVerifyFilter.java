f965d492fba43ab09fcec16068888b874acda2d1
txazo <784990655@qq.com>
2015-06-09 18:52:10 +0800

    weixin

---
+++ txazo-wx/weixin/src/main/java/org/txazo/weixin/filter/AgentVerifyFilter.java
@@ -0,0 +1,30 @@
+package org.txazo.weixin.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * AgentVerifyFilter
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @since 09.06.2015
+ */
+public class AgentVerifyFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
