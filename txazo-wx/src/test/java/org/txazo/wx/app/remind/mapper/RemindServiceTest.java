6742d3b19430d0d6db0e4ea0dc53dec13e1c0da3
txazo <784990655@qq.com>
2015-06-13 00:36:21 +0800

    remind

--- txazo-wx/src/test/java/org/txazo/wx/app/remind/mapper/RemindServiceTest.java
+++ txazo-wx/src/test/java/org/txazo/wx/app/remind/mapper/RemindServiceTest.java
@@ -23,10 +23,10 @@ public class RemindServiceTest extends SpringAbstractTest {
 
     @Test
     public void testAddRemind() {
-        Assert.assertTrue(remindService.addRemind(new Remind("txazo1218", "健康提醒", "喝水啦，休息一下吧/::P/::P/::P", "0 0 11,13,14,15,16,17,18 * * * ?")));
+        Assert.assertTrue(remindService.addRemind(new Remind("txazo1218", "健康提醒", "休息一下吧，顺便喝杯水/::P/::P/::P", "0 0 10,11,13,14,15,16,17,18,19,20 * * ?")));
         Assert.assertTrue(remindService.addRemind(new Remind("txazo1218", "睡眠提醒", "睡前吃个水果吧/::g/::g/::g", "0 0 21 * * ?")));
-        Assert.assertTrue(remindService.addRemind(new Remind("txazo1218", "运动提醒", "睡觉前，运动一下吧/:jump/:jump/:jump", "0 30 22 * * * ?")));
-        Assert.assertTrue(remindService.addRemind(new Remind("txazo1218", "睡眠提醒", "关电脑，睡觉啦/::Z/::Z/::Z", "0 30 23 * * ?")));
+        Assert.assertTrue(remindService.addRemind(new Remind("txazo1218", "运动提醒", "睡觉前，适量运动一下吧/:jump/:jump/:jump", "0 30 22 *  * ?")));
+        Assert.assertTrue(remindService.addRemind(new Remind("txazo1218", "睡眠提醒", "关上电脑，上床睡觉啦/::Z/::Z/::Z", "0 30 23 * * ?")));
     }
 
     @Test
