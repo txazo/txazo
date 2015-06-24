6cd664cd4d20e57f3ce18bc40e6d1bc28da430a3
txazo <784990655@qq.com>
2015-06-24 18:58:43 +0800

    update

---
+++ txazo-java/txazo-java-jvm/src/main/java/org/txazo/java/jvm/JVMArgs.java
@@ -0,0 +1,37 @@
+package org.txazo.java.jvm;
+
+import org.junit.Test;
+
+/**
+ * JVMArgs
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @since 24.06.2015
+ */
+public class JVMArgs {
+
+    /**
+     * VM Args: -client -server
+     */
+    @Test
+    public void clientServer() {
+    }
+
+    /**
+     * VM Args: -showversion
+     */
+    @Test
+    public void showVersion() {
+    }
+
+    /**
+     * VM Args: -server -showversion -Xint
+     * VM Args: -server -showversion -Xcomp
+     * VM Args: -server -showversion -Xmixed
+     */
+    @Test
+    public void mode() {
+    }
+
+}
