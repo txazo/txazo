009a0ec9681bf1383a43e947c9cf141f2e9ac774
txazo <784990655@qq.com>
2015-07-26 23:40:36 +0800

    update

---
+++ txazo-java/txazo-java-concurrent/src/main/java/org/txazo/java/concurrent/cas/AtomicIntegerJdk7.java
@@ -0,0 +1,47 @@
+package org.txazo.java.concurrent.cas;
+
+import sun.misc.Unsafe;
+
+/**
+ * AtomicIntegerJdk7
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @since 25.07.2015
+ */
+public class AtomicIntegerJdk7 {
+
+    private static final long valueOffset;
+    private static final Unsafe unsafe = Unsafe.getUnsafe();
+
+    static {
+        try {
+            valueOffset = unsafe.objectFieldOffset(AtomicIntegerJdk7.class.getDeclaredField("value"));
+        } catch (Exception ex) {
+            throw new Error(ex);
+        }
+    }
+
+    private volatile int value;
+
+    public AtomicIntegerJdk7() {
+    }
+
+    public AtomicIntegerJdk7(int initialValue) {
+        value = initialValue;
+    }
+
+    public final int get() {
+        return value;
+    }
+
+    public final void set(int newValue) {
+        value = newValue;
+    }
+
+    public final int incrementAndGet() {
+        return unsafe.getAndAddInt(this, valueOffset, 1) + 1;
+    }
+
+
+}
