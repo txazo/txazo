464aecd8b6ce3383a7e726ab1e0ab6c7424acfaf
txazo <784990655@qq.com>
2015-07-28 18:25:38 +0800

    update

---
+++ txazo-java/txazo-java-lang/src/main/java/org/txazo/java/lang/EqualsTest.java
@@ -0,0 +1,67 @@
+package org.txazo.java.lang;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * EqualsTest
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @see java.lang.Object
+ * @since 28.07.2015
+ */
+public class EqualsTest {
+
+    /**
+     * Object.equals()
+     *
+     * 1) ==比较引用是否相等
+     */
+
+    @Test
+    public void test() {
+        NotOverrideEquals notOverrideEquals = new NotOverrideEquals();
+        Assert.assertEquals(notOverrideEquals, notOverrideEquals);
+        Assert.assertNotEquals(notOverrideEquals, new NotOverrideEquals());
+        Assert.assertEquals(new OverrideEquals(), new OverrideEquals());
+        Assert.assertEquals(new OverrideEquals("cat"), new OverrideEquals("cat"));
+        Assert.assertNotEquals(new OverrideEquals("cat"), new OverrideEquals("get"));
+    }
+
+    /**
+     * 未重写equals(), 比较引用是否相等
+     */
+    private class NotOverrideEquals {
+
+    }
+
+    /**
+     * 重写equals()
+     */
+    private class OverrideEquals {
+
+        private String name;
+
+        public OverrideEquals() {
+        }
+
+        public OverrideEquals(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            OverrideEquals that = (OverrideEquals) o;
+            return name != null ? name.equals(that.name) : that.name == null;
+        }
+
+    }
+
+}
