979f553f6b537005c78590b5645a9b824fb4067e
txazo <784990655@qq.com>
2015-07-29 23:46:22 +0800

    update

--- txazo-java/txazo-java-collection/src/main/java/org/txazo/java/collection/package-info.java
+++ txazo-java/txazo-java-collection/src/main/java/org/txazo/java/collection/package-info.java
@@ -4,5 +4,7 @@
  * 2) Map
  * 3) 迭代器: Iterator Enumeration
  * 4) 工具类: Arrays Collections
+ *
+ * @see http://docs.oracle.com/javase/8/docs/technotes/guides/collections/reference.html
  */
 package org.txazo.java.collection;
