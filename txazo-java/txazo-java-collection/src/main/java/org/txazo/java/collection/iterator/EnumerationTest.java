5f08f5265217ee1a56b7b3bc4dcb2183ab83aa86
txazo <784990655@qq.com>
2015-07-30 17:33:59 +0800

    update

--- txazo-java/txazo-java-collection/src/main/java/org/txazo/java/collection/iterator/EnumerationTest.java
+++ txazo-java/txazo-java-collection/src/main/java/org/txazo/java/collection/iterator/EnumerationTest.java
@@ -1,8 +1,36 @@
 package org.txazo.java.collection.iterator;
 
+import org.junit.Test;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
 /**
- * Created by txazo on 15/7/28.
+ * EnumerationTest
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @see java.util.Enumeration
+ * @since 30.07.2015
  */
 public class EnumerationTest {
+
+    /**
+     * Enumeration
+     *
+     * 迭代器模式
+     */
+
+    @Test
+    public void test() {
+        Vector<Integer> vector = new Vector<Integer>();
+        for (int i = 0; i < 10; i++) {
+            vector.add(i);
+        }
+
+        for (Enumeration<Integer> e = vector.elements(); e.hasMoreElements(); ) {
+            System.out.println(e.nextElement());
+        }
+    }
 
 }
--- txazo-java/txazo-java-collection/src/main/java/org/txazo/java/collection/iterator/IteratorTest.java
+++ txazo-java/txazo-java-collection/src/main/java/org/txazo/java/collection/iterator/IteratorTest.java
@@ -1,8 +1,38 @@
 package org.txazo.java.collection.iterator;
 
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * Created by txazo on 15/7/28.
+ * IteratorTest
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @see java.util.Iterator
+ * @since 30.07.2015
  */
 public class IteratorTest {
+
+    /**
+     * Iterator
+     *
+     * 1) 迭代器
+     * 2) 迭代器模式
+     */
+
+    @Test
+    public void test() {
+        List<Integer> list = new LinkedList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        for (Iterator<Integer> i = list.iterator(); i.hasNext(); ) {
+            System.out.println(i.next());
+        }
+    }
 
 }
