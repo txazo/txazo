0626f16d1ad70cd3053de7a5dd2c7d3ff171bc13
txazo <784990655@qq.com>
2015-07-31 09:40:46 +0800

    update

---
+++ txazo-java/txazo-java-collection/src/main/java/org/txazo/java/collection/set/CopyOnWriteArraySetTest.java
@@ -0,0 +1,20 @@
+package org.txazo.java.collection.set;
+
+/**
+ * CopyOnWriteArraySetTest
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @see java.util.concurrent.CopyOnWriteArraySet
+ * @since 31.07.2015
+ */
+public class CopyOnWriteArraySetTest {
+
+    /**
+     * CopyOnWriteArraySet
+     *
+     * 1) 线程安全
+     * 2) 实现依赖CopyOnWriteArrayList
+     */
+
+}
--- txazo-java/txazo-java-collection/src/main/java/org/txazo/java/collection/set/HashSetTest.java
+++ txazo-java/txazo-java-collection/src/main/java/org/txazo/java/collection/set/HashSetTest.java
@@ -1,8 +1,20 @@
 package org.txazo.java.collection.set;
 
 /**
- * Created by txazo on 15/7/28.
+ * HashSetTest
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @see java.util.HashSet
+ * @since 31.07.2015
  */
 public class HashSetTest {
+
+    /**
+     * HashSet
+     *
+     * 1) 线程不安全
+     * 2) 实现依赖HashMap
+     */
 
 }
