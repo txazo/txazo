9714287a99cd09542ff3e15ac166d834a0120a7b
txazo <784990655@qq.com>
2015-08-04 23:31:03 +0800

    update

---
+++ txazo-application/txazo-blog/src/main/java/org/txazo/blog/common/A.java
@@ -0,0 +1,8 @@
+package org.txazo.blog.common;
+
+/**
+ * Created by txazo on 15/8/4.
+ */
+public class A {
+
+}
---
+++ txazo-application/txazo-blog/src/main/java/org/txazo/blog/web/A.java
@@ -0,0 +1,8 @@
+package org.txazo.blog.web;
+
+/**
+ * Created by txazo on 15/8/4.
+ */
+public class A {
+
+}
