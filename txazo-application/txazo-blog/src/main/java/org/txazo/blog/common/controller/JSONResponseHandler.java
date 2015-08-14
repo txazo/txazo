bb6c02783f5f22d5057ec999b899742872eec89f
txazo <784990655@qq.com>
2015-08-14 12:21:32 +0800

    blog

--- txazo-application/txazo-blog/src/main/java/org/txazo/blog/common/controller/JSONResponseHandler.java
+++ txazo-application/txazo-blog/src/main/java/org/txazo/blog/common/controller/JSONResponseHandler.java
@@ -1,8 +1,11 @@
 package org.txazo.blog.common.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import org.apache.commons.lang.StringUtils;
+import org.txazo.util.collection.map.MapUtils;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,28 +23,60 @@ public abstract class JSONResponseHandler extends ResponseHandler {
 
     protected enum ResponseStatus {
 
-        ERROR(0), SUCCESS(1);
+        ERROR(false), SUCCESS(true);
 
-        private int id;
+        private boolean status;
 
-        ResponseStatus(int id) {
-            this.id = id;
+        ResponseStatus(boolean status) {
+            this.status = status;
         }
 
-        public int getId() {
-            return id;
+        public boolean isStatus() {
+            return status;
         }
 
+    }
+
+    protected void responseJSON(String message) {
+        responseJson(message);
+    }
+
+    protected void responseJSON(Object data) {
+        responseJson(JSON.toJSONString(data));
+    }
+
+    protected void responseJSON(Collection collection) {
+        responseJson(JSONArray.toJSONString(collection));
+    }
+
+    protected void responseJSON(Map map) {
+        responseJson(JSONArray.toJSONString(map));
+    }
+
+    protected void responseJSON(Object... keyValuePairs) {
+        responseJSON(MapUtils.buildMap(keyValuePairs));
     }
 
     protected void responseJSON(ResponseStatus status) {
         responseJSON(status, null, null);
     }
 
+    protected void responseJSON(ResponseStatus status, String message) {
+        responseJSON(status, message, null);
+    }
+
+    protected void responseJSON(ResponseStatus status, Object data) {
+        responseJSON(status, null, data);
+    }
+
+    protected void responseJSON(ResponseStatus status, Object... keyValuePairs) {
+        responseJSON(status, null, MapUtils.buildMap(keyValuePairs));
+    }
+
     protected void responseJSON(ResponseStatus status, String message, Object data) {
         Map<String, Object> jsonMap = new HashMap<String, Object>();
         if (status != null) {
-            jsonMap.put("status", status.getId());
+            jsonMap.put("status", status.isStatus());
         }
         if (StringUtils.isNotBlank(message)) {
             jsonMap.put("message", message);
