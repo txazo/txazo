a1acbc303d5bf23023a6e9c2e0b0e75c991ae28b
txazo <784990655@qq.com>
2015-09-09 18:48:36 +0800

    update

---
+++ txazo-java/txazo-java-concurrent/src/main/java/org/txazo/java/concurrent/akka/MyActor.java
@@ -0,0 +1,19 @@
+package org.txazo.java.concurrent.akka;
+
+import akka.actor.UntypedActor;
+
+/**
+ * MyActor
+ *
+ * @author txazo
+ * @email txazo1218@163.com
+ * @since 2015-09-09
+ */
+public class MyActor extends UntypedActor {
+
+    @Override
+    public void onReceive(Object o) throws Exception {
+
+    }
+
+}
