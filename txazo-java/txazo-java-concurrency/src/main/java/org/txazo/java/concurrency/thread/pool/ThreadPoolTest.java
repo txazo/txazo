555fa1eb96f35071d07c8baae96013c71a483a51
txazo <784990655@qq.com>
2015-12-01 19:43:40 +0800

    update

--- txazo-java/txazo-java-concurrency/src/main/java/org/txazo/java/concurrency/thread/pool/ThreadPoolTest.java
+++ txazo-java/txazo-java-concurrency/src/main/java/org/txazo/java/concurrency/thread/pool/ThreadPoolTest.java
@@ -6,25 +6,25 @@ public class ThreadPoolTest {
 
     @Test
     public void test() throws InterruptedException {
-        ThreadPool pool = new DefaultThreadPool(10);
-        for (int i = 0; i < 100; i++) {
-            final int j = i;
-            pool.execute(new Runnable() {
-
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(j + " running ...");
-                }
-
-            });
-        }
-        Thread.sleep(20000);
-        pool.shutdown();
+//        ThreadPool pool = new DefaultThreadPool(10);
+//        for (int i = 0; i < 100; i++) {
+//            final int j = i;
+//            pool.execute(new Runnable() {
+//
+//                @Override
+//                public void run() {
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    System.out.println(j + " running ...");
+//                }
+//
+//            });
+//        }
+//        Thread.sleep(20000);
+//        pool.shutdown();
     }
 
 }
