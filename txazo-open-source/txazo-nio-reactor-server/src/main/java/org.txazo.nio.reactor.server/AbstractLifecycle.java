45d425ecf155fc68948bdb7e1fa018f21324a90b
txazo <784990655@qq.com>
2016-01-04 11:04:17 +0800

    update

---
+++ txazo-open-source/txazo-nio-reactor-server/src/main/java/org.txazo.nio.reactor.server/AbstractLifecycle.java
@@ -0,0 +1,29 @@
+package org.txazo.nio.reactor.server;
+
+public abstract class AbstractLifecycle implements Runnable, Lifecycle {
+
+    private volatile boolean running = true;
+
+    @Override
+    public void run() {
+        while (running && !Thread.interrupted()) {
+            try {
+                doRun();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    protected abstract void doRun() throws Exception;
+
+    @Override
+    public void start() throws Exception {
+    }
+
+    @Override
+    public void stop() throws Exception {
+        running = false;
+    }
+
+}
