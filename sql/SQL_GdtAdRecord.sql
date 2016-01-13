bbc1c128a66347dede81d23dcfdb404032821ca2
txazo <784990655@qq.com>
2016-01-13 19:35:07 +0800

    update

---
+++ sql/SQL_GdtAdRecord.sql
@@ -0,0 +1,16 @@
+create table GdtAdRecord (
+    id int(11) unsigned not null auto_increment comment 'id',
+    appid varchar(16) not null default '' comment '移动应用id',
+    appType varchar(8) not null default '' comment 'app类型',
+    advertiserId varchar(16) not null default '' comment '广告主id',
+    muid varchar(32) not null default '' comment '设备id',
+    clickId varchar(32) not null default '' comment '广告点击id',
+    clickTime varchar(16) not null default '' comment '广告点击时间',
+    ip varchar(16) not null default '' comment 'ip',
+    createTime datetime not null default '1970-01-01 00:00:00' comment '创建时间',
+    updateTime timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP comment '更新时间',
+    primary key (id),
+    unique (clickId),
+    key (createTime),
+    key (updateTime)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='广点通广告记录';
