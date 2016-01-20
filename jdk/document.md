9bcd26c3b32d973568dc0ec210593ad41d6ebe38
txazo <784990655@qq.com>
2016-01-20 18:07:46 +0800

    update

---
+++ jdk/document.md
@@ -0,0 +1,5 @@
+<https://www.jcp.org/>
+
+<http://docs.oracle.com/javase/specs/index.html>
+
+<http://openjdk.java.net/>
--- sql/SQL_GdtAdCallback.sql
+++ sql/SQL_GdtAdCallback.sql
@@ -1,20 +1,3 @@
-create table GdtAdRecord (
-    id int(11) unsigned not null auto_increment comment 'id',
-    appid varchar(16) not null default '' comment '移动应用id',
-    appType varchar(8) not null default '' comment 'app类型',
-    advertiserId varchar(16) not null default '' comment '广告主id',
-    muid varchar(32) not null default '' comment '设备id',
-    clickId varchar(32) not null default '' comment '广告点击id',
-    clickTime varchar(16) not null default '' comment '广告点击时间',
-    ip varchar(16) not null default '' comment 'ip',
-    createTime datetime not null default '1970-01-01 00:00:00' comment '创建时间',
-    updateTime timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP comment '更新时间',
-    primary key (id),
-    unique (clickId),
-    key (createTime),
-    key (updateTime)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='广点通广告记录';
-
 create table GdtAdCallback (
     id int(11) unsigned not null auto_increment comment 'id',
     appid varchar(16) not null default '' comment '移动应用id',
@@ -25,6 +8,7 @@ create table GdtAdCallback (
     clickTime varchar(16) not null default '' comment '广告点击时间',
     ip varchar(16) not null default '' comment 'ip',
     activeTime varchar(16) not null default '' comment '激活时间',
+    dpid varchar(32) not null default '' comment 'dpid',
     ret tinyint(4) not null default -1 comment '返回码',
     msg varchar(32) not null default '' comment '错误提示',
     createTime datetime not null default '1970-01-01 00:00:00' comment '创建时间',
